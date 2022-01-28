988a7a3ee25a805254cd64a7399c0be41373eea0
Harsh-Korat <95098291+Harsh-Korat@users.noreply.github.com>
2022-01-28 23:30:35 +0530

    Add files via upload

---
+++ Helperland/assets/js/Homepage.js
@@ -0,0 +1,9 @@
+// JavaScript for navbar
+
+window.addEventListener('scroll', function () {
+let nav = document.querySelector('nav');
+let windowPosition = window.scrollY > 0;
+nav.classList.toggle('scrolling-active', windowPosition);
+});
+
+
