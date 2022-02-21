f207565792717a94e4fa20aa43e63deb5a97bcbc
Harsh-Korat <95098291+Harsh-Korat@users.noreply.github.com>
2022-02-21 08:06:12 +0530

    Update Helperland-model.php

--- Helperland/Models/Helperland-model.php
+++ Helperland/Models/Helperland-model.php
@@ -84,6 +84,41 @@ public function __construct() {
     }
 
 
+    public function Login1($email, $password)
+    {
+        $base_url = "http://localhost/Helperland/#LoginModal";
+        $customer = "http://localhost/Helperland/Views/book-service.php";
+        $sp = "http://localhost/Helperland/Views/book-service.php";
+
+        $sql = "select * from user where Email = '$email'";
+        $stmt =  $this->conn->prepare($sql);
+        $stmt->execute();
+        $row  = $stmt->fetch(PDO::FETCH_ASSOC);
+        $count = $stmt->rowCount();
+        $usertypeid = $row['UserTypeId'];
+        $Password = $row['Password'];
+        if ($count == 1) {
+            if (password_verify($password, $Password)) {
+                if ($usertypeid == 0) {
+                    $_SESSION['username'] = $email;
+                    header('Location:' . $customer);
+                    
+                } else if ($usertypeid == 1) {
+                    $_SESSION['username'] = $email;
+                    header('Location:' . $sp);
+                }
+            } else {
+                $_SESSION['msg'] = "Password Invalid.  Please Enter Valid Password.";
+
+                header('Location:' . $base_url);
+            }
+        } else {
+            $_SESSION['msg'] = "User does not exists.  Please Enter Valid User Name.";
+
+            header('Location:' . $base_url);
+        }
+    }
+
 
 
 
