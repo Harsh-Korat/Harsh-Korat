7baa43756010cbee835023ea7bf076226ba1f252
Harsh-Korat <95098291+Harsh-Korat@users.noreply.github.com>
2022-03-30 09:17:08 +0530

    Modify

--- Helperland/Views/index.php
+++ Helperland/Views/index.php
@@ -6,7 +6,7 @@
    
     <link rel="stylesheet" href="../assets/css/validation.css">
     <link rel="stylesheet" href="../assets/css/nav-headers.css">
-    <link rel="stylesheet" href="../assets/css/home-style.css">
+    <link rel="stylesheet" href="../assets/css/home-styles.css">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
